1d4f5a19beeb82c8cd9d5e4d6f0d7ad87fa6dfec
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-06-10 11:55:18 -0300

    Utilizado RXJS

--- gatitobook/src/app/animais/lista-animais/lista-animais.component.ts
+++ gatitobook/src/app/animais/lista-animais/lista-animais.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Animais } from '../animais';
 import { UsuarioService } from 'src/app/autenticacao/usuario/usuario.service';
 import { AnimaisService } from '../animais.service';
+import { switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-lista-animais',
@@ -10,7 +12,7 @@ import { AnimaisService } from '../animais.service';
 })
 export class ListaAnimaisComponent implements OnInit {
 
-  animais!: Animais;
+  animais$ !: Observable<Animais>;
 
   constructor(
     private usuarioService: UsuarioService,
@@ -18,11 +20,11 @@ export class ListaAnimaisComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.usuarioService.retornaUsuario().subscribe((usuario) => {
-      const userName = usuario.name ?? '';
-      this.animaisService.listaDoUsuario(userName).subscribe((animais) => {
-        this.animais = animais;
-      });
-    });
+    this.animais$ = this.usuarioService.retornaUsuario().pipe(
+      switchMap((usuario) => {
+        const userName = usuario.name ?? '';
+        return this.animaisService.listaDoUsuario(userName);
+      })
+    );
   }
 }
